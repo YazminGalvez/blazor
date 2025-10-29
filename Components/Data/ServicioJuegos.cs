c77f3d67f206fc0c93f9a1a386a97b6070ba30da
Yazmin Galvez <yazgalvez1515@gmail.com>
2025-10-28 21:51:05 -0700

    Agregar archivos de proyecto.

---
+++ Components/Data/ServicioJuegos.cs
@@ -0,0 +1,26 @@
+﻿namespace blazor.Components.Data
+{
+    public class ServicioJuegos
+    {
+        private List<Juego> juegos = new List<Juego>
+        {
+            new Juego{Identificador=1,Nombre="Ravel", Jugado=false},
+            new Juego{Identificador=2,Nombre="Carcassonne", Jugado=true}
+        };
+        public Task<List<Juego>> ObtenerJuegos() => Task.FromResult(juegos);
+
+        public Task AgregarJuego(Juego juego)
+        {
+            juegos.Add(juego);
+            return Task.CompletedTask;
+        }
+        public void CambiarEstadoJugado(Juego juego)
+        {
+            var juegoExistente = juegos.FirstOrDefault(j => j.Identificador == juego.Identificador);
+            if (juegoExistente != null)
+            {
+                juegoExistente.Jugado = !juegoExistente.Jugado;
+            }
+        }
+    }
+}
